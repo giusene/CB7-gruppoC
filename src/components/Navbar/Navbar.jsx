a1b8410f1b4bf3cb3b59fa000b621307ed95a7de
MatteoNorr <matteo.norrito@outlook.com>
2023-07-25 23:37:11 +0200

    feat: Google authentication with Firebase

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,30 @@
 import styles from "./Navbar.module.scss";
+import { app, auth, provider } from "@/plugins/firebase";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const Navbar = () => {
+  const signIn = async () => {
+    const res = await signInWithPopup(auth, provider).then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      const user = result.user;
+      console.log(credential);
+      return user;
+    });
+    // .catch((error) => {
+    //   // Handle Errors here.
+    //   const errorCode = error.code;
+    //   const errorMessage = error.message;
+    //   // The email of the user's account used.
+    //   const email = error.customData.email;
+    //   // The AuthCredential type that was used.
+    //   const credential = provider.credentialFromError(error);
+    //   // ...
+    // });
+    // console.log(res);
+    // return res;
+  };
+
   return (
     <ul className={styles.Navbar}>
       <img
@@ -9,7 +33,7 @@ const Navbar = () => {
       />
       <img className={styles.logoImg} src="https://img.logoipsum.com/296.svg" />
       <input className={styles.input} type="text" placeholder="Search..." />
-      <button>Login</button>
+      <button onClick={signIn}>Login</button>
     </ul>
   );
 };
