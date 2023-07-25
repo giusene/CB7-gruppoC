29a5020ececed8bdbe185955060d2033e4933f6a
martinapiras <131855775+martinapiras@users.noreply.github.com>
2023-07-25 16:14:23 +0200

    fix: fetch data by category and by query

--- src/utils/HTTP.js
+++ src/utils/HTTP.js
@@ -1,9 +1,9 @@
 const BASE_URL = process.env.NEXT_PUBLIC_TMDB_BASE_URL;
 const API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
 
-export const GET = async (type, endpoint, genre = "") => {
+export const GET = async (type, endpoint, genre = "", query = "") => {
   const res = await fetch(
-    `${BASE_URL}${type}/${endpoint}?api_key=${API_KEY}&language=en-US&append_to_response=credits,videos,images&include_image_language=en,null&with_genres=${genre}`
+    `${BASE_URL}${type}${endpoint}?api_key=${API_KEY}&language=en-US&append_to_response=credits,videos,images&include_image_language=en,null&with_genres=${genre}&query=${query}`
   );
 
   const data = res.json();
