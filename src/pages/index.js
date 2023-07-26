43904218e8642bb6986bd164969b06c7d6c090bd
MatteoNorr <matteo.norrito@outlook.com>
2023-07-26 21:52:50 +0200

    fix: Hero import on HomePage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,11 @@ import { Inter } from "next/font/google";
 import { GET } from "@/utils/HTTP";
 import Cards from "@/components/Cards/Cards";
 import styles from "@/styles/Home.module.scss";
+import Hero from "@/components/hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home({ trending, pippo }) {
-
- 
-
-
   return (
     <>
       <Head>
@@ -19,14 +16,11 @@ export default function Home({ trending, pippo }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Hero trending={trending} />
-
       <main>
         <Cards data={trending} />
         <Cards data={trending} />
       </main>
-
     </>
   );
 }
